0753b0f9768c8b863f96243145dbd0c6a6caba17
pechevarria <pechevarria05@gmail.com>
2023-08-21 00:34:02 -0500

    configuracion de menu

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [],
+})
+export class MenuComponent implements OnInit {
+  public items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro Elemento',
+            icon: 'pi pi-cog',
+          },
+        ],
+      },
+    ];
+  }
+}
