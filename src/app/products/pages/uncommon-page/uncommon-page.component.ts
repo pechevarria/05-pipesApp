71d069816be8ee1096f5420606beee7fb7987f5a
pechevarria <pechevarria05@gmail.com>
2023-08-27 01:52:15 -0500

    pipes angular

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -18,4 +19,36 @@ export class UncommonPageComponent {
     this.name = 'Melissa';
     this.gender = 'female';
   }
+
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 personas esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //KeyValue
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada',
+  };
+
+  public myObservableTimer = interval(2000).pipe(
+    tap((value) => console.log('tap:', value))
+  );
 }
